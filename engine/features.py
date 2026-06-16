37912cf971dda41819c5ae249b312e6e732862bf
prathmesh2507 <prathmeshbhoyar321@gmail.com>
2026-06-16 14:00:00 +0530

    added hotword detection for wakeword jarvis

--- engine/features.py
+++ engine/features.py
@@ -1,12 +1,18 @@
 import os
 import re
+import struct
+import time
 from playsound import playsound
 import eel
+import pyaudio
 from engine.command import speak
 from engine.config import ASSISTANT_NAME
 import pywhatkit as kit
 import webbrowser
 import sqlite3
+import pvporcupine
+
+from engine.helper import extract_yt_term
 
 
 conn = sqlite3.connect('jarvis.db')
@@ -63,7 +69,40 @@ def PlayYoutube(query):
     kit.playonyt(search_term)
     
 
-def extract_yt_term(command):
-    pattern = r'play\s+(.*?)\s+on\s+youtube'
-    match = re.search(pattern, command, re.IGNORECASE)
-    return match.group(1) if match else None
+def hotword():
+    porcupine=None
+    paud=None
+    audio_stream=None
+    try:
+       
+        # pre trained keywords    
+        porcupine=pvporcupine.create(keywords=["jarvis","alexa"]) 
+        paud=pyaudio.PyAudio()
+        audio_stream=paud.open(rate=porcupine.sample_rate,channels=1,format=pyaudio.paInt16,input=True,frames_per_buffer=porcupine.frame_length)
+        
+        # loop for streaming
+        while True:
+            keyword=audio_stream.read(porcupine.frame_length)
+            keyword=struct.unpack_from("h"*porcupine.frame_length,keyword)
+
+            # processing keyword comes from mic 
+            keyword_index=porcupine.process(keyword)
+
+            # checking first keyword detetcted for not
+            if keyword_index>=0:
+                print("hotword detected")
+
+                # pressing shorcut key win+j
+                import pyautogui as autogui
+                autogui.keyDown("win")
+                autogui.press("j")
+                time.sleep(2)
+                autogui.keyUp("win")
+                
+    except:
+        if porcupine is not None:
+            porcupine.delete()
+        if audio_stream is not None:
+            audio_stream.close()
+        if paud is not None:
+            paud.terminate()
