078ec41670463261e89e24ab1b6c762ed4b0cdb1
prathmesh2507 <prathmeshbhoyar321@gmail.com>
2026-06-17 14:00:00 +0530

    now jarvis can run using hotword,shortcutkeys and physically also

--- main.py
+++ main.py
@@ -4,15 +4,10 @@ from engine.features import *
 from engine.command import *
 
 
-eel.init('www')
-
-playAssistantSound()
-
-
-
-
-
-os.system('start msedge.exe --app=http://localhost:8000/index.html')
-
-eel.start('index.html', mode=None , host='localhost', block = True)
+def start():
+    eel.init('www')
+    playAssistantSound()
+    
+    os.system('start msedge.exe --app=http://localhost:8000/index.html')
+    eel.start('index.html', mode=None , host='localhost', block = True)
 
