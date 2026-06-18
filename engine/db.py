cc8a9f474149b9b5c268f510546b54654d3de2de
prathmesh2507 <prathmeshbhoyar321@gmail.com>
2026-06-18 14:00:00 +0530

    Ignore Test Files

--- engine/db.py
+++ engine/db.py
@@ -1,18 +1,18 @@
-import sqlite3
+#import sqlite3
 
-conn = sqlite3.connect('jarvis.db')
-cursor = conn.cursor()
+#conn = sqlite3.connect('jarvis.db')
+#cursor = conn.cursor()
 
-query = "CREATE TABLE IF NOT EXISTS sys_command (id INTEGER PRIMARY KEY, name varchar(100), path varchar(1000))"
-cursor.execute(query)
+#query = "CREATE TABLE IF NOT EXISTS sys_command (id INTEGER PRIMARY KEY, name varchar(100), path varchar(1000))"
+#cursor.execute(query)
 
 #query = "insert into sys_command values (null, 'vs code', 'C:\\Users\\Asus\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe')"
 #cursor.execute(query)
 #conn.commit()
 
-query = "CREATE TABLE IF NOT EXISTS web_command (id INTEGER PRIMARY KEY, name varchar(100), url varchar(1000))"
-cursor.execute(query)
+#query = "CREATE TABLE IF NOT EXISTS web_command (id INTEGER PRIMARY KEY, name varchar(100), url varchar(1000))"
+#cursor.execute(query)
 
-query = "insert into web_command values (null, 'github', 'https://www.github.com/')"
-cursor.execute(query)
-conn.commit()
+#query = "insert into web_command values (null, 'github', 'https://www.github.com/')"
+#cursor.execute(query)
+#conn.commit()
