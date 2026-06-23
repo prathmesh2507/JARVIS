c7e93d5c49008851bf676ff6ec6da7843fe50fb6
prathmesh2507 <prathmeshbhoyar321@gmail.com>
2026-06-23 14:00:00 +0530

    query for adding contacts from csv file to our db

--- engine/db.py
+++ engine/db.py
@@ -1,3 +1,4 @@
+import csv
 import sqlite3
 
 conn = sqlite3.connect('jarvis.db')
@@ -19,4 +20,19 @@ cursor = conn.cursor()
 
 
 # Create a table with the desired columns
-cursor.execute('''CREATE TABLE IF NOT EXISTS contacts (id integer primary key, name VARCHAR(200), mobile_no VARCHAR(255), email VARCHAR(255) NULL)''')
+#cursor.execute('''CREATE TABLE IF NOT EXISTS contacts (id integer primary key, name VARCHAR(200), mobile_no VARCHAR(255), email VARCHAR(255) NULL)''')
+
+
+
+desired_columns_indices = [0, 30]
+
+# Read data from CSV and insert into SQLite table for the desired columns
+with open('contacts.csv', 'r', encoding='utf-8') as csvfile:
+    csvreader = csv.reader(csvfile)
+    for row in csvreader:
+        selected_data = [row[i] for i in desired_columns_indices]
+        cursor.execute(''' INSERT INTO contacts (id, 'name', 'mobile_no') VALUES (null, ?, ?);''', tuple(selected_data))
+
+# Commit changes and close connection
+conn.commit()
+conn.close()
