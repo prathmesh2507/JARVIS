13796764b7d0fe2a3115ac7f41f2823471543f2d
prathmesh2507 <prathmeshbhoyar321@gmail.com>
2026-06-25 14:00:00 +0530

    created remove_words helper function

--- engine/db.py
+++ engine/db.py
@@ -22,17 +22,6 @@ cursor = conn.cursor()
 # Create a table with the desired columns
 #cursor.execute('''CREATE TABLE IF NOT EXISTS contacts (id integer primary key, name VARCHAR(200), mobile_no VARCHAR(255), email VARCHAR(255) NULL)''')
 
+# Optional: Clear old wrong data
+#cursor.execute("DELETE FROM contacts")
 
-
-desired_columns_indices = [0, 30]
-
-# Read data from CSV and insert into SQLite table for the desired columns
-with open('contacts.csv', 'r', encoding='utf-8') as csvfile:
-    csvreader = csv.reader(csvfile)
-    for row in csvreader:
-        selected_data = [row[i] for i in desired_columns_indices]
-        cursor.execute(''' INSERT INTO contacts (id, 'name', 'mobile_no') VALUES (null, ?, ?);''', tuple(selected_data))
-
-# Commit changes and close connection
-conn.commit()
-conn.close()
