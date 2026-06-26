d82c0dc3ebc25af1bc0ff928a573b0f5b27f08b7
prathmesh2507 <prathmeshbhoyar321@gmail.com>
2026-06-26 14:00:00 +0530

    created find contact function

--- engine/features.py
+++ engine/features.py
@@ -1,10 +1,13 @@
 import os
 import re
+from shlex import quote
 import struct
+import subprocess
 import time
 from playsound import playsound
 import eel
 import pyaudio
+import pyautogui
 from engine.command import speak
 from engine.config import ASSISTANT_NAME
 import pywhatkit as kit
@@ -12,7 +15,8 @@ import webbrowser
 import sqlite3
 import pvporcupine
 
-from engine.helper import extract_yt_term
+
+from engine.helper import extract_yt_term, remove_words
 
 
 conn = sqlite3.connect('jarvis.db')
@@ -106,3 +110,24 @@ def hotword():
             audio_stream.close()
         if paud is not None:
             paud.terminate()
+
+def findContact(query):
+    
+    
+    words_to_remove = [ASSISTANT_NAME, 'make', 'a', 'to', 'phone', 'call', 'send', 'message', 'wahtsapp', 'video']
+    query = remove_words(query, words_to_remove)
+
+    try:
+        query = query.strip().lower()
+        cursor.execute("SELECT mobile_no FROM contacts WHERE LOWER(name) LIKE ? OR LOWER(name) LIKE ?", ('%' + query + '%', query + '%'))
+        results = cursor.fetchall()
+        print(results[0][0])
+        mobile_number_str = str(results[0][0])
+        if not mobile_number_str.startswith('+91'):
+            mobile_number_str = '+91' + mobile_number_str
+
+        return mobile_number_str, query
+    except:
+        speak('not exist in contacts')
+        return 0, 0
+
